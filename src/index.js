3c54ed6d410b44010dca0c62b45620e37c42dbec
Yuriy <agent250691@yandex.ru>
2020-10-06 18:02:25 +0300

    feat: add Morse solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const len = expr.length / 10;
+    let arr = [];
+    for (let i = 0; i < len; i++) {
+        arr.push(expr.slice(i * 10, i * 10 + 10));
+    }
+    let word = [];
+    
+    for (let el of arr) {
+        if (el !== '**********') {
+            let letter = '';
+            let l = '';
+            for (let j = 0; j < 5; j++) {
+                
+                l = el.slice(j * 2, j * 2 + 2);
+                if (l === '00') continue;
+                if (l === '10') letter += '.';
+                if (l === '11') letter += '-';
+            }
+            word.push(MORSE_TABLE[letter]);
+            letter = '';
+
+        } else {
+            word.push(' ');
+        }
+    }
+
+    return word.join('');
 }
 
 module.exports = {
